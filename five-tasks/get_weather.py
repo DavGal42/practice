daa5ade92ad3de93d9fc6db344044c608f433909
David Galstyan <Galstyand.25@gmail.com>
2024-04-25 13:59:15 +0400

    Added weather task

--- five-tasks/get_weather.py
+++ five-tasks/get_weather.py
@@ -1,11 +1,35 @@
 import requests
 
 api_key = '4ce14ec96659e7a0d805d06e96444648'
-city_name = 'London,uk'
 
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+def get_city_name():
+    name = input('Enter a city name: ')
+    return name
 
-response = requests.get(url)
-data = response.json()
 
-print(data)
+def get_data(key, city_name):
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    return data
+
+
+def check_city_name():
+    while True:
+        city_name = get_city_name()
+        data = get_data(api_key, city_name)
+        if data == {'cod': '404', 'message': 'city not found'}:
+            print('There is no such city.Try again')
+            continue
+        return data
+
+
+def main():
+    answer = check_city_name()
+    print(answer)
+
+if __name__ == '__main__':
+    main()
